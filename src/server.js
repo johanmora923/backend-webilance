394b976f526f26fa04f1600681716a37438b3fbe
johanmora923 <johanmora923@gmail.com>
2025-03-14 22:26:49 -0400

    Update CORS configuration to allow requests from production URL and add .env to .gitignore

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ console.log("servidor corriendo", app.get("port"));
 
 // Configuración de CORS
 const corsOptions = {
-    origin: "http://localhost:5173", // Permitir solicitudes desde este origen
+    origin: ["http://localhost:5173","https://webbilance.vercel.app" ],// Permitir solicitudes desde este origen
     methods: "GET, POST", // Métodos permitidos
     credentials: true, // Permitir el uso de cookies
 };
@@ -33,7 +33,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173","https://webbilance.vercel.app");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
